c1203fc93955a53cde92dbab1a67bbb391e6bfd6
trevrobb <97711911+trevrobb@users.noreply.github.com>
2023-10-22 22:42:13 -0400

    added simple UI

--- Robbins-3258-Midterm/Assets/Scripts/GameManager.cs
+++ Robbins-3258-Midterm/Assets/Scripts/GameManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int score;
 
     public static GameManager instance;
+    [SerializeField] TextMeshProUGUI _scoreText;
+    [SerializeField] TextMeshProUGUI _bulletsText;
+
 
     public int bullets;
     void Start()
@@ -20,6 +25,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _scoreText.text = "Score: " + score;
+        _bulletsText.text = "Bullets: " + bullets;
+
     }
 }
