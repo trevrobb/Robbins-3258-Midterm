b9cf24a25aa852110992eef314905a7d6f68427e
trevrobb <97711911+trevrobb@users.noreply.github.com>
2023-10-22 22:10:22 -0400

    reload scene when FOV enemy touches

--- Robbins-3258-Midterm/Assets/FOVEnemy.cs
+++ Robbins-3258-Midterm/Assets/FOVEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class FOVEnemy : MonoBehaviour
 {
@@ -19,6 +20,14 @@ public class FOVEnemy : MonoBehaviour
         if (_playerInFOV)
         {
             this.GetComponent<NavMeshAgent>().SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
